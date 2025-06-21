9ea21a33c663b44b9c5e0ea0bcfa68cc92cca906
unknown <PC21@MAAPCB02LC3021.duoc.acad>
2025-06-21 10:28:42 -0400

    Programa de python para calcular promedio.

---
+++ promedio.py
@@ -0,0 +1,21 @@
+contador = 0
+lista_notas = []
+while contador < 5:
+    try:
+        nota = int(input(f"Ingresa tu nota N°{contador+1}: "))
+        if nota >= 1 and nota <= 7:
+            lista_notas.append(nota)
+            contador += 1
+        else:
+            print("El rango de notas permitido es 1-7.")
+    except:
+        print("Valor invalido. Solo puede ingresar numeros.")
+
+def calcular_promedio():
+    suma = 0
+    for i in(lista_notas):
+        suma += i
+    promedio = suma / len(lista_notas)
+    print(f"El promedio de tus notas es {promedio}.")
+
+calcular_promedio()
